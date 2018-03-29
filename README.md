0f25cd2e15868afcdc211bafe7ea5fb8a48fae23
haoran <wanghaoran@qudian.com>
2018-03-29 14:56:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,80 @@
+{
+  "rightQrcode": {
+    "img": "./static/imgs/qrcode.png"  // 右侧二维码
+  },
+  "swiper": [
+    {
+      "link": "baidu.com",    // 轮播图的点击跳转链接
+      "img": "http://static.ydcss.com/uploads/ydui/2.jpg"    // 轮播图的图片链接
+    },
+    {
+      "link": "baidu.com",
+      "img": "http://static.ydcss.com/uploads/ydui/2.jpg"
+    }
+  ],
+  "tabs": [{
+    "name": "全部列表",   // 第一个tab的名字
+    "topDownload": [
+      {
+        "link": "javascript:;", // 顶部的广告位的链接
+        "img": "./static/imgs/top-download.png" // 顶部的广告位的图片地址（参考这个路径，图片放在./static/imgs/下面）
+      },
+      {
+        "link": "javascript:;",
+        "img": "./static/imgs/top-download.png"
+      },
+      {
+        "link": "javascript:;",
+        "img": "./static/imgs/top-download.png"
+      }
+    ],
+    "list": [{
+      "name": "火币网",       // 名字
+      "logo": "./static/imgs/huobi.png",  // logo图片
+      "detail": "支持人民币比特币交易平台",   // 详情介绍
+      "cornerImg": "./static/imgs/corner.png",  //  左上角角标
+      "androidImg": "./static/imgs/qrcode.png",  // 安卓二维码
+      "iosImg": "./static/imgs/qrcode.png",   // ios二维码
+      "iosLink": "baidu.com" // ios下载链接
+       "androidLink": "baidu.com" // 安卓下载链接
+    }]
+  }, {
+    "name": "排行榜",  // 第儿个tab的名字
+    "topDownload": [
+      {
+        "link": "javascript:;",
+        "img": "./static/imgs/top-download.png"
+      },
+      {
+        "link": "javascript:;",
+        "img": "./static/imgs/top-download.png"
+      },
+      {
+        "link": "javascript:;",
+        "img": "./static/imgs/top-download.png"
+      }
+    ],
+    "list": [{
+      "name": "排行榜测试",
+      "logo": "./static/imgs/LOGO.png",
+      "detail": "让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……",
+      "iosLink": "baidu.com",
+      "androidLink": "google.com"
+    }]
+  }, {
+    "name": "下载送币"
+  }],
+  "footerImg": "./static/imgs/footer.png",  // 最底下的背景图
+  "qrcodes": [
+    {
+      "name": "微信二维码",  // 底下的二维码名字
+      "img": "./static/imgs/qrcode.png" // 底下的二维码图片
+    },
+    {
+      "name": "微信san维码",
+      "img": "./static/imgs/qrcode.png"
+    }
+  ],
+  "footerInfo": "商务合作方式：13466581111     QQ群：239026794" // 最底部文字
+}
+
---
+++ static/config/mobConf.js
@@ -0,0 +1,102 @@
+const config = {
+  rightQrcode: {
+    img: './static/imgs/qrcode.png'
+  },
+  swiper: [
+    {
+      link: 'baidu.com',
+      img: 'http://static.ydcss.com/uploads/ydui/2.jpg'
+    },
+    {
+      link: 'baidu.com',
+      img: 'http://static.ydcss.com/uploads/ydui/2.jpg'
+    }
+  ],
+  tabs: [{
+    name: '全部列表',
+    topDownload: [
+      {
+        link: 'javascript:;',
+        img: './static/imgs/top-download.png'
+      },
+      {
+        link: 'javascript:;',
+        img: './static/imgs/top-download.png'
+      },
+      {
+        link: 'javascript:;',
+        img: './static/imgs/top-download.png'
+      }
+    ],
+    list: [{
+      name: '火币网',
+      logo: './static/imgs/huobi.png',
+      detail: '支持人民币比特币交易平台',
+      cornerImg: './static/imgs/corner.png',
+      androidImg: './static/imgs/qrcode.png',
+      iosImg: './static/imgs/qrcode.png'
+    }, {
+      name: '全部列表测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……'
+    }, {
+      name: '全部列表测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……'
+    }, {
+      name: '全部列表测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……'
+    }, {
+      name: '全部列表测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……'
+    }, {
+      name: '全部列表测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……'
+    }, {
+      name: '全部列表测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……'
+    }]
+  }, {
+    name: '排行榜',
+    topDownload: [
+      {
+        link: 'javascript:;',
+        img: './static/imgs/top-download.png'
+      },
+      {
+        link: 'javascript:;',
+        img: './static/imgs/top-download.png'
+      },
+      {
+        link: 'javascript:;',
+        img: './static/imgs/top-download.png'
+      }
+    ],
+    list: [{
+      name: '排行榜测试',
+      logo: './static/imgs/LOGO.png',
+      detail: '让我们共创顶级全球化语音AI解决方案们共创顶级全球化语音AI……',
+      iosLink: 'baidu.com',
+      androidLink: 'google.com'
+    }]
+  }, {
+    name: '下载送币'
+  }],
+  footerImg: './static/imgs/footer.png',
+  qrcodes: [
+    {
+      name: '微信二维码',
+      img: './static/imgs/qrcode.png'
+    },
+    {
+      name: '微信san维码',
+      img: './static/imgs/qrcode.png'
+    }
+  ],
+  footerInfo: '商务合作方式：13466581111     QQ群：239026794'
+}
+export default config
